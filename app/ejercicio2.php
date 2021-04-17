46cbdf4f9fa1e375f13313c3171d8939094254e6
Pivano Fernando <fpivano@tmoviles.com.ar>
2021-04-16 22:09:43 -0300

    ejercicio 2

--- app/ejercicio2.php
+++ app/ejercicio2.php
@@ -1,7 +1,7 @@
 
 <?php
 function iva ($importe){
-	$iva = ($importe * 1.16);
+	$iva = ($importe * 1.21);
 	return $iva;
 }
 
@@ -10,5 +10,4 @@ $importe_con_iva = iva ($importe);
 
 echo "Importe: $importe<br />";
 echo "Importe + IVA: $importe_con_iva";
-}
 ?>
