a5866e8b3b80cb75361eaa53200aaff3f78588c3
Pivano Fernando <fpivano@tmoviles.com.ar>
2021-04-22 15:13:34 -0300

    ejercicio4

---
+++ app/ejercicio4.php
@@ -0,0 +1,20 @@
+
+<?php
+$num1 = 10;
+$num2 = 6;
+$num3 = 5;
+
+If($num1 > $num2){
+If($num1 > $num3){
+echo 'numero ' . $num1 . ' es mayor';
+} else {
+echo 'numero ' . $num3 . ' es mayor';
+}
+} else {
+If($num2 > $num3){
+echo 'numero ' . $num2 . ' es mayor';
+} else {
+echo 'numero ' . $num3 . ' es mayor';
+}
+}
+?>
