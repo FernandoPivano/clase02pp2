0a94f9f879fd4a245b1680cd5eb8cf6467afab3f
Pivano Fernando <fpivano@tmoviles.com.ar>
2021-04-23 19:03:12 -0300

    ejercicio 4

--- app/ejercicio4.php
+++ app/ejercicio4.php
@@ -5,9 +5,9 @@
 	$num2 = rand (1,50);
 	$num3 = rand (1,50);
 
-	echo  "El numero 1 es: $ num1 <br>";
-	echo  "El numero 2 es: $ num2 <br>";
-	echo  "El numero 3 es: $ num3 <br>";
+	echo  "El numero 1 es: $num1 <br>";
+	echo  "El numero 2 es: $num2 <br>";
+	echo  "El numero 3 es: $num3 <br>";
 
  
 	if ($num1 > $num2) {
@@ -19,5 +19,5 @@
 	if ($num3 > $variable) {
 		$variable = $num3;
 	}
-	echo  "El mayor es: $ mayor" ;
+	echo  "El mayor es: $variable";
 ?>
