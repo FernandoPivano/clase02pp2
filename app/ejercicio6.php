a106e47ea81fe7de72f4ac2208be6958426acbe1
Pivano Fernando <fpivano@tmoviles.com.ar>
2021-04-23 21:48:47 -0300

    ejercicio 6

---
+++ app/ejercicio6.php
@@ -0,0 +1,19 @@
+
+<?php
+
+ //Bariloche = Hace Frio
+ //Cataratas = Hace Calor
+
+ $Bariloche = 1;
+ $Cataratas = 2;
+
+ if ( $Bariloche == 1 ) {// Primer IF
+ echo "Hace Frio";
+ } elseif( $Cataratas == 2 ) {// Segundo IF
+ echo "Hace Calor";
+ }else{// Valor falso
+ echo "No hace nada";
+ }
+  
+
+?>
