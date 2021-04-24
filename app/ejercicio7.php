90f52b05ff6e44a57163e1392b7b7cb60cceda04
Pivano Fernando <fpivano@tmoviles.com.ar>
2021-04-23 21:24:55 -0300

    ejercicio 7

--- app/ejercicio7.php
+++ app/ejercicio7.php
@@ -7,24 +7,25 @@ $num3 = rand (1,50);
 
 echo "El número 1 es: $num1 <br>";
 echo "El número 2 es: $num2 <br>";
-echo "El número 3 es: $num3 <br>";
+echo "El número 3 es: $num3 <br> <br>";
 
-if ($num1!=$num2 && $num1!=$num3 && $num2!=$num3) {
+if ($num1<>$num2 && $num1<>$num3 && $num2<>$num3) {
 
-     if($num1>$num2 && $num2<$num3) {
-          $medio=$num2;
+     if(($num1 > $num2 && $num1 < $num3) || ($num1 < $num2 && $num1 > $num3)) 	{
+          $medio=$num1;
      } else {
-          if ($num1>$num3 && $num3<$num2) {
-               $medio=$num3;
-          } else {
-           $medio=$num1;
+          if (($num2 > $num1 && $num2 < $num3) || ($num2 < $num1 &&  $num2 > $num3)) {
+               $medio=$num2;
+          } else 	{
+               if (($num3 > $num1 && $num3 < $num2) || ($num3 < $num1 && $num3 > $num2)){
+                $medio=$num3;					
+               }
+
           }
      }
      $medio;
-
-}
-else 
-{
+     echo "$num1, $num2, $num3 el medio es : $medio";
+}else {
      echo "$num1, $num2, $num3 no tiene";
 }
 
